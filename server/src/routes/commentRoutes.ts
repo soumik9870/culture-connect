5a0b44b75f650c79b923e77d73933202d74be2d7
Soumik Saha <soumiksaha827@gmail.com>
2025-07-21 21:57:20 +0530

    update comment route

--- server/src/routes/commentRoutes.ts
+++ server/src/routes/commentRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createComment, getComments } from '../controllers/commentController';
+import { protect } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-router.post('/', createComment);
-router.get('/', getComments);
+router.post('/', protect, createComment);
+router.get('/', protect, getComments);
 
 export default router;
